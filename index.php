042f26fbb8477d2693c4bbebe1c7e52b828dd5da
pachopascolat <patriciopascolat@gmail.com>
2019-04-09 19:38:38 -0300

    maquetado pagina 1

--- index.php
+++ index.php
@@ -1,198 +1,159 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+ <?php
+require("header.php");
+ ?>
+ 
+    <div class="row">
+			<div class="col-md-6">
+				<table class="table table-hover table-sm table-info text-center">
+					<thead >
+						<tr>
+							<th>
+								#
+							</th>
+							<th>
+								Barcos
+							</th>
+							<th>
+								Coordenadas
+							</th>
+							<th>
+								Alineacion
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr >
+							<td>
+								1
+							</td>
+							<td>
+								Barco 1
+							</td>
+							<td>
 
-    <title>Bootstrap 4, from LayoutIt!</title>
+								<input class="form-control" type="text">
+								-
+								<input class="form-control" type="text">
 
-    <meta name="description" content="Source code generated using layoutit.com">
-    <meta name="author" content="LayoutIt!">
+							</td>
+							<td>
+								<select class="form-control">
+									<option value="horizontal">Horizontal</option>
+									<option value="vertical">Vertical</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								2
+							</td>
+							<td>
+								Barco 2
+							</td>
+							<td>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+								<input class="form-control" type="text">-<input class="form-control"
+									type="text">
 
-  </head>
-  <body>
+							</td>
+							<td>
+								<select class="form-control">
+									<option value="horizontal">Horizontal</option>
+									<option value="vertical">Vertical</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								3
+							</td>
+							<td>
+								Barco 3
+							</td>
+							<td >
 
-    <div class="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<h3>
-				h3. Lorem ipsum dolor sit amet.
-			</h3>
+								<input class="form-control " type="text">-<input class="form-control"
+									type="text">
+
+							</td>
+							<td>
+								<select class="form-control">
+									<option value="horizontal">Horizontal</option>
+									<option value="vertical">Vertical</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								4
+							</td>
+							<td>
+								Barco 4
+							</td>
+							<td >
+
+								<input class="form-control" type="text">-<input class="form-control"
+									type="text">
+
+							</td>
+							<td>
+								<select class="form-control">
+									<option value="horizontal">Horizontal</option>
+									<option value="vertical">Vertical</option>
+								</select>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								5
+							</td>
+							<td>
+								Barco 5
+							</td>
+							<td >
+
+								<input class="form-control" type="text">-<input class=" form-control"
+									type="text">
+
+							</td>
+							<td>
+								<select class="form-control">
+									<option value="horizontal">Horizontal</option>
+									<option value="vertical">Vertical</option>
+								</select>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="col-md-6">
+				<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
+			</div>
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
-			<ul class="nav">
-				<li class="nav-item">
-					<a class="nav-link active" href="#">Home</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Profile</a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link disabled" href="#">Messages</a>
-				</li>
-				<li class="nav-item dropdown ml-md-auto">
-					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-						<div class="dropdown-divider">
-						</div> <a class="dropdown-item" href="#">Separated link</a>
+		<div class="row">
+			<div class="col-md-6">
+				<form role="form">
+					<div class="form-group">
+
+						<label for="inputUsuario">
+							Usuario
+						</label>
+						<input type="text" class="form-control" id="inputUsuario">
 					</div>
-				</li>
-			</ul>
-			<div class="row">
-				<div class="col-md-12">
-				</div>
+					
+					<button type="submit" class="btn btn-primary">
+						Submit
+					</button>
+				</form>
+			</div>
+			<div class="col-md-6">
 			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="col-md-6">
-			<table class="table table-hover table-sm">
-				<thead>
-					<tr>
-						<th>
-							#
-						</th>
-						<th>
-							Product
-						</th>
-						<th>
-							Payment Taken
-						</th>
-						<th>
-							Status
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Default
-						</td>
-					</tr>
-					<tr class="table-active">
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Approved
-						</td>
-					</tr>
-					<tr class="table-success">
-						<td>
-							2
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							02/04/2012
-						</td>
-						<td>
-							Declined
-						</td>
-					</tr>
-					<tr class="table-warning">
-						<td>
-							3
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							03/04/2012
-						</td>
-						<td>
-							Pending
-						</td>
-					</tr>
-					<tr class="table-danger">
-						<td>
-							4
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							04/04/2012
-						</td>
-						<td>
-							Call in to confirm
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="col-md-6">
-			<img alt="Bootstrap Image Preview" src="https://www.layoutit.com/img/sports-q-c-140-140-3.jpg">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-6">
-			<form role="form">
-				<div class="form-group">
-					 
-					<label for="exampleInputEmail1">
-						Email address
-					</label>
-					<input type="email" class="form-control" id="exampleInputEmail1">
-				</div>
-				<div class="form-group">
-					 
-					<label for="exampleInputPassword1">
-						Password
-					</label>
-					<input type="password" class="form-control" id="exampleInputPassword1">
-				</div>
-				<div class="form-group">
-					 
-					<label for="exampleInputFile">
-						File input
-					</label>
-					<input type="file" class="form-control-file" id="exampleInputFile">
-					<p class="help-block">
-						Example block-level help text here.
-					</p>
-				</div>
-				<div class="checkbox">
-					 
-					<label>
-						<input type="checkbox"> Check me out
-					</label>
-				</div> 
-				<button type="submit" class="btn btn-primary">
-					Submit
-				</button>
-			</form>
-		</div>
-		<div class="col-md-6">
-		</div>
-	</div>
-</div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/scripts.js"></script>
-  </body>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/scripts.js"></script>
+</body>
+
 </html>
